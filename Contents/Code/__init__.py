def91f68929af883ce9ea7b451dc58ba568a286c
sander1 <sander@plexapp.com>
2013-08-01 02:15:42 +0200

    Fix for Classics.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -10,7 +10,7 @@ CATEGORIES = [
 ]
 
 RE_S_EP_DURATION = Regex('(S(\d+) Ep(\d+) )?\((\d+:\d+)\)')
-RE_SAFE_TITLE = Regex('/classics/(.+?)/video')
+RE_SAFE_TITLE = Regex('/shows/([^/]+)')
 RE_SEASON = Regex('Season ([0-9]+),')
 
 EXCLUDE_SHOWS = ('CBS Evening News', 'Live on Letterman')
